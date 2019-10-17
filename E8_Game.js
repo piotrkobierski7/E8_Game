e35b9fe771b741bade04bf05be5b5d132c8cb4b9
Piotr Kobierski <piotr.kobierski7@gmail.com>
2019-10-17 16:51:32 +0200

    Stage 2

--- E8_Game.js
+++ E8_Game.js
@@ -1,3 +1,5 @@
+var resultPlayer=0;
+var resultComp=0;
 button_Rock.addEventListener('click', function() {
 	playerMove(1,0,0);
 });
@@ -23,13 +25,17 @@ function judgePrint(rock,paper,scissor,rockComp,paperComp,scissorComp){
 	var output = document.getElementById('output');
 	if((rock==1&&scissorComp==1)||(paper==1&&rockComp==1)||(scissor==1&&paperComp==1)){
 		output.insertAdjacentHTML('afterbegin', 'You have won.<br>');//win conditions
+		resultPlayer++;
+		document.getElementById('result').innerHTML='Player`s result '+resultPlayer+' | Computer`s result'+resultComp+'<br><br>' ;
 	}
 	else{
 		if((rock==1&&paperComp==1)||(paper==1&&scissorComp==1)||(scissor==1&&rockComp==1)){
 		output.insertAdjacentHTML('afterbegin', 'You have lost.<br>');//lose conditions
+		resultComp++;
+		document.getElementById('result').innerHTML='Player`s result '+resultPlayer+' | Computer`s result'+resultComp+'<br><br>' ;
 		}else{
 			output.insertAdjacentHTML('afterbegin', 'There is a draw.<br>');//draw conditions
-
+			document.getElementById('result').innerHTML='Player`s result '+resultPlayer+' | Computer`s result'+resultComp+'<br><br>' ;
 			}
 	}
 }
